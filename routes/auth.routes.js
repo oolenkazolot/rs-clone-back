faf416db4385dc892f7c52339deacebf3e7a4c1f
oolenkazolot <oolenka.zolot@gmail.com>
2023-02-06 09:52:16 +0100

    feat: implement return token on registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,14 +8,12 @@ const router = Router();
 
 // /api/auth/register
 router.post('/register', [check('email', 'Incorrect email').isEmail(), check('password', 'Minimum length 6 characters').isLength({ min: 6 })], async (req, res) => {
-  
   try {
     const errors = validationResult(req);
-    console.log(req.body);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
-        massage: 'Incorrect data during registration',
+        message: 'Incorrect data during registration',
       });
     }
     const { email, password } = req.body;
@@ -27,11 +25,11 @@ router.post('/register', [check('email', 'Incorrect email').isEmail(), check('pa
 
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = new User({ email, password: hashedPassword });
-
     await user.save();
-    res.status(201).json({ message: 'User created' });
+    const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), { expiresIn: '1h' });
+    res.json({ token, userId: user.id });
   } catch (e) {
-    res.status(500).json({ massage: 'Something went wrong, please try again' });
+    res.status(500).json({ message: 'Something went wrong, please try again' });
   }
 });
 
@@ -42,7 +40,7 @@ router.post('/login', [check('email', 'Please enter a valid email').normalizeEma
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
-        massage: 'Incorrect login details',
+        message: 'Incorrect login details',
       });
     }
 
@@ -50,7 +48,7 @@ router.post('/login', [check('email', 'Please enter a valid email').normalizeEma
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ massage: 'Invalid data, please try again' });
+      return res.status(400).json({ message: 'Invalid data, please try again' });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
@@ -63,7 +61,7 @@ router.post('/login', [check('email', 'Please enter a valid email').normalizeEma
 
     res.json({ token, userId: user.id });
   } catch (e) {
-    res.status(500).json({ massage: 'Something went wrong, please try again' });
+    res.status(500).json({ message: 'Something went wrong, please try again' });
   }
 });
 
