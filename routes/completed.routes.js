20f6ddef7157f0a041aefe1fad5819966f76b47a
oolenkazolot <oolenka.zolot@gmail.com>
2023-02-26 17:03:47 +0100

    feat: create weekly-workouts

--- routes/completed.routes.js
+++ routes/completed.routes.js
@@ -1,9 +1,10 @@
-const Complex = require('../models/Complex');
+const UserInfo = require('../models/UserInfo');
 const Completed = require('../models/Completed');
 const { Router } = require('express');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
+const moment = require('moment'); // require
 
 // /api/completed/create
 // передаётся userId, уникальный id выполненного комплекса (если не user создавал complex, то id complex из json файла, ), time число в минутах
@@ -22,7 +23,7 @@ router.post('/create', [check('userId', 'Missing userId').exists(), check('idCom
     const user = await User.findOne({ _id: userId });
 
     if (user) {
-      const completed = new Completed({ userId, idComplex, date: new Date(), time });
+      const completed = new Completed({ userId, idComplex, date: new Date('2023-02-27'), time });
       await completed.save();
       return res.json(completed);
     } else {
@@ -35,7 +36,7 @@ router.post('/create', [check('userId', 'Missing userId').exists(), check('idCom
   }
 });
 
-// /api/completed/get-all
+// /api/completed/get-all/:id
 
 //получить количество выполненных complexes и общее время, затраченное на выполненные комплексы
 //в пути указываем userId
@@ -73,6 +74,51 @@ function calcTotalTime(completed) {
     totalTime.minutes = totalTimeMinutes % 60;
   }
   return totalTime;
+}
+
+// /api/completed/weekly-workouts/:id
+
+//получить количество выполненных complexes и общее время, затраченное на выполненные комплексы
+//в пути указываем userId
+router.get('/weekly-workouts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const completed = await Completed.find({
+      userId: id,
+      date: {
+        $gt: moment().startOf('isoWeek'),
+        $lt: moment().endOf('isoWeek'),
+      },
+    });
+
+    const userInfo = await UserInfo.findOne({ userId: id });
+
+    const resultData = {
+      weeklyWorkouts: [],
+      goal: '0',
+    };
+
+    if (userInfo) {
+      resultData.goal = userInfo.goal;
+    }
+
+    if (completed) {
+      resultData.weeklyWorkouts = getWeeklyWorkouts(completed);
+    }
+    return res.json(resultData);
+  } catch (e) {
+    res.json({ message: 'Something went wrong, please try again' });
+  }
+});
+
+function getWeeklyWorkouts(completed) {
+  let setWeeklyWorkouts = new Set();
+  completed.forEach((completedComplex) => {
+    const numberDate = moment(completedComplex.date).isoWeekday();
+    setWeeklyWorkouts.add(numberDate);
+  });
+  return [...setWeeklyWorkouts].sort();
 }
 
 module.exports = router;
