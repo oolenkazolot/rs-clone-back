1e1b0346f1838163daf5f4bc2be539445777feaf
oolenkazolot <102587224+oolenkazolot@users.noreply.github.com>
2023-02-28 19:27:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api/complex', require('./routes/complex.routes'));
 app.use('/api/exercise', require('./routes/exercise.routes'));
 app.use('/api/completed', require('./routes/completed.routes'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 mongoose.set('strictQuery', false);
 
 async function start() {
